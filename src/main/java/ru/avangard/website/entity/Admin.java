a4100cbefb23be36e5de93df951d31ba07c74d46
damirbik <dbikchurayev@bk.ru>
2025-12-30 21:45:00 +0300

    db update

--- src/main/java/ru/avangard/website/entity/Admin.java
+++ src/main/java/ru/avangard/website/entity/Admin.java
@@ -19,6 +19,30 @@ public class Admin {
     @Column(unique = true, nullable = false)
     private String login;
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     @Column(nullable = false)
     private String password; // хранить в виде хеша!
 }
